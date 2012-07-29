6894d6eef5daba38f5d29d1d2dc37a4b045ca323
Lasse Dahl Ebert <lasseebert@gmail.com>
2012-07-29 22:13:11 +0200

    working on move and validate

--- lib/pulver_gammon/board.rb
+++ lib/pulver_gammon/board.rb
@@ -4,25 +4,108 @@ module PulverGammon
     # The board in the form:
     # {black: [checkers], white: [checkers]}
     # Checkers is an array of length 25:
-    # Index 0: Prison
-    # Index 1-24: Normal fields where 19-24 is the home board
+    # Index 0-23: Normal fields where 0-5 is the home board
+    # Index 24: Prison
     attr_accessor :fields
 
     # The next player to move
     attr_accessor :turn
 
     def initialize(start_turn)
-      self.fields = {black: Array.new(25, 0), white: Array.new(25, 0)}
-      fields[:black][1] = 2
-      fields[:black][12] = 5
-      fields[:black][17] = 3
-      fields[:black][19] = 5
-      fields[:white][1] = 2
-      fields[:white][12] = 5
-      fields[:white][17] = 3
-      fields[:white][19] = 5
+      self.fields = {black: [], white: []}
+      checkers = {
+        5 => 5,
+        7 => 3,
+        12 => 5,
+        23 => 2
+      }
+      (0..24).each do |index|
+        amount = checkers[index] || 0
+        fields[:black] << amount
+        fields[:white] << amount
+      end
 
       self.turn = start_turn
     end
+
+    # Moves one dice roll
+    # We need to validate the entire roll to take care of situation 
+    # where both dice can be used individually but not at the same time
+    # A move is a pair: [from_field, amount]
+    def move(moves, dice)
+      return false unless validate_move(moves, dice)
+
+      turn_fields = fields[turn]
+      other_color = turn == :black ? :white : :black
+      other_fields = fields[other_color]
+
+      moves.each do |move|
+        Board.move_internal(move, turn_fields, other_fields)
+      end
+
+      self.turn = other_color
+    end
+
+    # Returns a boolean indicating if the move is legal
+    def validate_move(moves, dice)
+      turn_fields = fields[turn].dup
+      other_color = turn == :black ? :white : :black
+      other_fields = fields[other_color].dup
+
+      moves.each do |move|
+  
+        from_field = move[0]
+        amount = move[1]
+
+        to_field = from_field - amount
+        other_to_field = 23 - to_field
+
+        # No checker on field
+        return false unless turn_fields[from_field] > 0
+
+        # Hitting wall
+        return false unless to_field < 0 || other_fields[other_to_field] < 2
+
+        # Not moving prison checker
+        return false if turn_fields[24] > 0 && from_field != 24
+
+        # Bearing off
+        if to_field < 0
+          return false if last_checker(turn) > 5
+          return false if to_field < -1 && from_field != last_checker(turn)
+        end
+
+        # Make the move on the dupes
+        Board.move_internal(move, turn_fields, other_fields)
+      end
+      true
+    end
+
+    private 
+
+    def last_checker(color)
+      (0..24).to_a.reverse.each do |index|
+        return index if fields[index] > 0
+      end
+      -1
+    end
+
+    def self.move_internal(move, turn_fields, other_fields)
+
+      from_field = move[0]
+      amount = move[1]
+      to_field = from_field - amount
+      other_to_field = 23 - to_field
+
+      turn_fields[from_field] -= 1
+      unless to_field < 0
+        turn_fields[to_field] += 1
+        if other_fields[other_to_field] == 1
+          other_fields[other_to_field] = 0
+          other_fields[24] += 1
+        end
+      end
+    end
+
   end
 end
--- test/pulver_gammon/board_test.rb
+++ test/pulver_gammon/board_test.rb
@@ -3,26 +3,152 @@ require 'pulver_gammon/board'
 
 class TestBoard < Test::Unit::TestCase
   
+  def setup
+    @board = PulverGammon::Board.new(:black)
+  end
+
+  def set_board(black = {}, white = {})
+    @board.fields = { black: [], white: [] }
+    (0..24).each do |index|
+      @board.fields[:black] << (black[index] || 0)
+      @board.fields[:white] << (white[index] || 0)
+    end
+  end
+
   def test_initialize
     expected_fields = {
-      1 => 2,
+      5 => 5,
+      7 => 3,
       12 => 5,
-      17 => 3,
-      19 => 5
+      23 => 2
     }
 
-    board = PulverGammon::Board.new(:black)
-    
-    assert_equal 2, board.fields.length
-    assert_equal 25, board.fields[:white].length
-    assert_equal 25, board.fields[:black].length
+    assert_equal 2, @board.fields.length
+    assert_equal 25, @board.fields[:white].length
+    assert_equal 25, @board.fields[:black].length
     (0..24).each do |index|
-      assert_equal expected_fields[index] || 0, board.fields[:white][index]
-      assert_equal expected_fields[index] || 0, board.fields[:black][index]
+      assert_equal expected_fields[index] || 0, @board.fields[:white][index]
+      assert_equal expected_fields[index] || 0, @board.fields[:black][index]
     end
 
-    assert_equal :black, board.turn
+    assert_equal :black, @board.turn
 
+  end
+
+
+  def test_move
+    @board.move([[5, 2], [12, 4]], [2, 4])
+
+    assert_equal 1, @board.fields[:black][3]
+    assert_equal 4, @board.fields[:black][5]
+    assert_equal 1, @board.fields[:black][8]
+    assert_equal 4, @board.fields[:black][12]
+
+    assert_equal :white, @board.turn
+  end
+
+  def test_move_hit
+    set_board({9 => 1, 10 => 1}, {17 => 1})
+
+    @board.move([[9, 3], [10, 4]], [3, 4])
+
+    assert_equal 0, @board.fields[:white][17]
+    assert_equal 2, @board.fields[:black][6]
+    assert_equal 0, @board.fields[:black][9]
+    assert_equal 0, @board.fields[:black][10]
+  end
+
+  def test_move_home
+    set_board({2 => 2, 3 => 2}, {17 => 15})
+
+    @board.move([[3, 4], [2, 3]], [3, 4])
+
+    assert_equal 1, @board.fields[:black][2]
+    assert_equal 1, @board.fields[:black][3]
+  end
+
+
+  def test_validate_true
+    assert @board.validate_move([[5, 2], [12, 4]], [2, 4])
+  end
+
+  def test_validate_missing_checker
+    assert_false @board.validate_move([[13, 3]], [3])
+  end
+
+  def test_validate_not_match_dice
+    assert_false @board.validate_move([[12, 3]], [4])
+  end
+
+  def test_validate_hitting_wall
+    assert_false @board.validate_move([[12, 1]], [1])
+  end
+  def test_validate_missing_hitting_single
+    set_board({5 => 1}, {19 => 1})
+    assert @board.validate_move([[5, 1]], [1])
+  end
+
+  def test_validate_not_using_largest_dice_amount
+    set_board({ 5 => 1 }, { 22 => 2, 23 => 2 })
+    assert_false @board.validate_move([[5, 2]], [2, 3])
+
+    set_board({ 5 => 1 }, { 22 => 2, 23 => 2 })
+    assert @board.validate_move([[5, 3]], [2, 3])
+  end
+
+  def test_validate_not_use_all_dice
+    assert_false @board.validate_move([[12, 3]], [2, 3])
+  end
+
+  def test_validate_use_all_dice_on_double
+    assert @board.validate_move([[12, 3], [12, 3], [12, 3], [12, 3]], [3, 3])
+  end
+  def test_validate_not_use_all_dice_on_double
+    assert_false @board.validate_move([[12, 3], [12, 3], [12, 3]], [3, 3])
+  end
+
+  def test_validate_not_moving_prison_checker
+    set_board({23 => 1, 24 => 1})
+    assert_false @board.validate_move([[23, 1]], [1])
+  end
+
+  def test_validate_moving_prison_checker
+    set_board({23 => 1, 24 => 1})
+    assert @board.validate_move([[24, 1]], [1])
+  end
+
+  def test_validate_no_moves
+    set_board({24 => 1}, { 0 => 2})
+    assert @board.validate_move([], [1, 1])
+  end
+
+  def test_validate_move_checker_twice
+    assert @board.validate_move([[12, 2], [10, 4]], [2, 4])
+  end
+
+  def test_validate_should_not_move_board
+    assert @board.validate_move([[12, 2], [10, 4]], [2, 4])
+    assert_equal 5, @board.fields[:black][12]
+  end
+
+  def test_validate_bear_off_with_checkers_outside_home
+    set_board({0 => 1, 6 => 1})
+    assert_false @board.validate_move([[0, 1]], [1])
+  end
+
+  def test_validate_bear_off_without_checkers_outside_home
+    set_board({0 => 1, 5 => 1})
+    assert @board.validate_move([[0, 1]], [1])
+  end
+
+  def test_validate_bear_off_with_too_large_dice_correct
+    set_board({0 => 1, 1 => 1})
+    assert_false @board.validate_move([[0, 6]], [6])
+  end
+
+  def test_validate_bear_off_with_too_large_dice_incorrect
+    set_board({0 => 1, 1 => 1})
+    assert @board.validate_move([[1, 6]], [6])
   end
 
 end
